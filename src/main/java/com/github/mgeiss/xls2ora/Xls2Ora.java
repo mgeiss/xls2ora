2fec8677831a446aad7092be679f49fc53ed65dc
Markus Geiss <mgeiss257@googlemail.com>
2012-06-19 17:15:08 +0200

    First class added.

---
+++ src/main/java/com/github/mgeiss/xls2ora/Xls2Ora.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012 Markus Geiss.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.mgeiss.xls2ora;
+
+/**
+ * <code>Xls2Ora</code> is a wizard based utility to import a sheet from a
+ * .xls File into an Oracle Database. It is possible to rename the columns
+ * and set a default value before importing.
+ * 
+ * @author Markus Geiss
+ * @version 1.0
+ */
+public class Xls2Ora {
+
+    public Xls2Ora() {
+        super();
+    }
+    
+    public static void main(String[] args) {
+        Xls2Ora xls2Ora = new Xls2Ora();
+    }
+}
